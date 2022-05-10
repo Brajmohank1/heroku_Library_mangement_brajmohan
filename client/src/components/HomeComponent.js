27ae4a18026b01e03682bd694a4fe09a093356f3
Brajmohan <brajmohanst9661@gmail.com>
2022-05-10 17:16:03 +0530

    b

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -17,23 +17,21 @@ render(){
         <div className="container mt-4 home text-center align-self-center">
         <br/><br/><br/>
             <div className="row mt-3 darkbg text-center justify-content-center">
-            <h1 align="center"> Welcome to the Central Library</h1>
+            <h1 align="center"> Welcome to the TMSL Library</h1>
             </div>
             <div className="row darkbg">
 
         <br/><br/><br/>
         <br/><br/><br/>
             <h6>
-            The Central Library of Indian Institute of Technology (Indian School of Mines) Dhanbad is the heart of the institute providing direct academic and research supports to all departments.
-
-            The Central Library is an automated library in terms of records organisation and management of all its different sections, search and discovery, information retrieval and service delivery. The whole library operations run on LIBSYS integrated library management software.
+            This library management system is software that is designed to manage all the functions of a library. It helps librarian to maintain the database of new books and the books that are borrowed by members along with their due dates. This system completely automates all your library's activities.
             <br/><br/> </h6>
             </div>
             <div className="row darkbg justify-content-center">
             <table>
     <tr> <th colspan="2"><h6>Library Timings</h6></th> </tr>
     <tr> <td><h6>Opening Time</h6> </td> <td> <h6>9.00 A.M.</h6></td></tr>
-    <tr> <td>Closing Time </td> <td> 9.00 P.M.</td></tr>
+    <tr> <td>Closing Time </td> <td> 5.00 P.M.</td></tr>
             </table>
             <br/>
             <br/>
