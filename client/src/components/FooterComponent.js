27b95bd1089e1b5f389fad8d4d7e8800989a1e54
Brajmohan <brajmohanst9661@gmail.com>
2022-05-10 14:45:44 +0530

    hosting - braj

---
+++ client/src/components/FooterComponent.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class Footer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+        }
+    }
+
+render(){
+    return(
+                    <p className="footer" align="center">© Copyright 2020-21 Brajmohan Kumar Singh</p>
+          );
+}
+
+}
+
+export default Footer;
